a7a04a440b2901d95a922bb24de93c66ed76a96b
Yisheng Jiang <yisheng.jiang@gmail.com>
2019-01-09 18:12:44 -0800

    ghetto css

--- resources/views/terminal.blade.php
+++ resources/views/terminal.blade.php
@@ -5,7 +5,12 @@
   <link href="{{ asset('css/cmd.css') }}" rel="stylesheet">
   <link href="{{ asset('css/modal.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-
+  <style>
+  pre{
+    background-color:black;
+    color:white;
+  }
+  </style>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
