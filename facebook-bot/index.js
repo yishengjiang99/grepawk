d609aed29d5479c2d5c445a61969cd3925aeb0fd
Yisheng Jiang <yisheng.jiang@gmail.com>
2019-01-18 09:13:10 -0800

    cleanup

--- facebook-bot/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
-
-const mountRoutes = require('./routes')
-mountRoutes(app)
-
-app.listen(834)
