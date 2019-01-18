0b1caa2a65cf366bf8c584b6405b9c154ce78b43
Yisheng Jiang <yisheng.jiang@gmail.com>
2019-01-18 15:18:32 -0800

    ..

--- node/fs.js
+++ node/fs.js
@@ -1,96 +1,31 @@
 const net = require('net');
-const fs = require('fs');
-var path = require('path');
+const path = require('path');
 const dirname = path.resolve(__dirname);
-const base_path = path.dirname(dirname)+"/";
+const base_path = path.dirname(dirname);
+const map_path = base_path+"/map/";
+const fs = require("fs");
+var world = {};
 
-
-const appDirectory = fs.realpathSync(process.cwd());
-
-//post csv
-
-var userProfiles={};
-// List all files in a directory in Node.js recursively in a synchronous fashion
-var walkSync = function(dir,filelist,level) {
-  var fs = fs || require('fs'),
-      files = fs.readdirSync(dir);
-       
-  level = level || 0;
-  filelist = filelist || [];
-  filename = path.basename(dir);
-  filelist.push({'level':level,'is_dir':1, 'filename':filename,'path':dir});
-  files.forEach(function(file) {
-    var stat = fs.statSync(dir + file);
-    if (stat.isDirectory()) {
-      filelist = walkSync(dir + file + '/', filelist,level+1);
-    //  filelist.push({'path':dir,'is_dir':true, 'size':stat.size,'filename':file,'created':stat.birthtimeMs,'updated':stat.atimeMs});
-      filelist.push({'level':level+1,'filename':file,'path':dir+file,'is_dir':1,'size':stat.size,'created':new Date(stat.birthtimeMs),'updated':new Date(stat.atimeMs)});
-    }
-    else {
-      filelist.push({'level':level+1,'filename':file,'path':dir+file,'is_dir':0,'size':stat.size,'created':new Date(stat.birthtimeMs),'updated':new Date(stat.atimeMs)});
-    }
-  });
-  return filelist;
-}
-
-
-var fs_cache=[];
-var g_filename_to_path_map=false;
-
-var init=function(){//
-  if(g_filename_to_path_map) return g_filename_to_path_map;
-  g_filename_to_path_map={};
-  //fs_cache=walkSync(data_path,fs_cache);
-  fs_cache=walkSync(base_path,fs_cache);
-  fs_cache.forEach(function(file){
-    filename= file.filename in g_filename_to_path_map ? file.filename+".2" : file.filename;
-    console.log("adding "+filename);
-    g_filename_to_path_map[filename]=file;
-  });
-  return g_filename_to_path_map;
-}
-
-var ls=function(filename,max_level){
-  filename=filename||"";
-  max_level=max_level||10;
-
-  filename_to_path_map=init();
-
-  filename=filename.toLowerCase();
-
-  var file_list=[];
-  var max_level=max_level;
-  Object.keys(filename_to_path_map).map(function(file_name,index){   
-
-    var _file=filename_to_path_map[file_name];
-    if(!_file) return;
-    if(_file.filename.indexOf(filename)>-1 || _file.path.indexOf(filename)>-1){
-
-      if(_file.level < max_level+1){
-        file_list.push(file_name);
-      }
-    }
+async function load_dir(path){
+  new Promise((resolve,reject)=>{
+    fs.readdir(path,(err,files)=>{
+      if(err) reject(err);
+      else    resolve(files);
+    })
+  }).then((files)=>{
+    console.log(files)
+  }).catch((err)=>{
+    console.error(err)
   })
-}
 
-var ls_output=function(file_list){
-  var retstr="";
-  file_list.forEach(function(file_name,index){
-    var fileobj = filename_to_path_map[file_name];
-    if(fileobj['is_dir']){
-      retstr+="<span style='color:blue;margin-right:20px'><b>"+file_name+"</b></span>";
-    }else{
-      retstr+="<span style='color:white'>"+file_name+"</span><br>";
-    }
-  });
-  return retstr;
+}
+async function init_fs(){
+  var zones = load_dir(map_path);
+  console.log(zones);
 }
 
 
-
-init();
-console.log("wrirting ls");
-ls();
+init_fs().catch(console.error)
 
 const server = net.createServer((socket) => {
   socket.write("Hello to file server!\n");
@@ -123,6 +58,6 @@ const server = net.createServer((socket) => {
 });
 
 // grab an arbitrary unused port.
-server.listen(4010);
+server.listen(4000);
 
 //
