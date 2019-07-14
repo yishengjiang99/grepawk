bcb0ed5d1271a37fb31452648690361b35c9ac9e
Yisheng Jiang <yisheng.jiang@gmail.com>
2019-07-14 15:43:52 -0700

    dangerous

---
+++ public/js/user.js
@@ -0,0 +1,36 @@
+function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+        d += performance.now(); //use high-precision timer if available
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+var User = function(){
+  var uuid;
+  var cwd;
+  var xp;
+  var points;
+  var get_uuid = function(){
+    if (uuid) return uuid;
+    if (uuid = localStorage.getItem('uuid')) return uuid;
+    
+    uuid = generateUUID();
+    localStorage.setItem('uuid', uuid);
+    return uuid;
+  }
+  var get_info = function(){
+    
+  }
+ 
+  return {
+    get_info: get_info,
+    get_uuid: get_uuid
+  }
+}
+
+
--- ws.js
+++ ws.js
@@ -1,22 +1,48 @@
 const WebSocket = require('ws')
 const port = process.env.port || 8081
 const wss = new WebSocket.Server({ port: port })
-const { exec, spawn } = require('child_process');
+const { exec, spawn,execSync } = require('child_process');
 
 console.log("listening on "+port)
 
-wss.on('connection', ws => {
+var User = {
+  getInfo : function(uuid){
+    return {xp:1}
+  }
+}
+var users  = {};
+wss.on('connection', (ws,request) => {
   ws.on('message', message => {
     try{
       message = message.trim();
-      var cmd = spawn(message);
-
-      cmd.unref();
-      cmd.stdout.on('data',(data)=>{
-        console.log(data.toString('utf8'));
-        ws.send(data.toString('utf8'));
-      })
+      var t = message.split(" ");
+      if(t ==="") return;
+    
+      const cmd = t[0];
+      const args =t.length>1 ? t.splice(1) : [];
+  
+      switch(cmd){
+        case 'check-in':
+          const uuid  = args[0];
+         
+          users[uuid] = {
+            ws: ws,
+            info: User.getInfo(uuid)
+          }
+          console.log(users); 
+          break;
+        case 'ls':
+          exec(message, (err, stdout, stderr)=>{
+              if(err) ws.send("error: "+err.message);
+              else ws.send(stdout);
+          });
+          break;
+        default:
+          ws.send(message);
+          break;
+      }
     }catch(err){
+      console.log(err);
       ws.send(err.message);
     }
   })
