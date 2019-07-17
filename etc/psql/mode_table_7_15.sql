d82fe88d8def5e912cdf2d27bd7824457a5d218e
Yisheng Jiang <yisheng.jiang@gmail.com>
2019-07-16 22:14:54 -0700

    quest db

---
+++ etc/psql/mode_table_7_15.sql
@@ -0,0 +1,4 @@
+alter table users add username text default null;
+alter table users add password text default null;
+create table quest_completion (uuid text not null, quest_id numeric not null);
+alter table quests add pwd text;
