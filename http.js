16274dea1ff700882b8dea3b6afe9344e5eee750
Yisheng Jiang <yisheng.jiang@gmail.com>
2019-07-23 18:28:45 -0700

    stuff

--- http.js
+++ http.js
@@ -6,8 +6,8 @@ const db =require("./lib/db");
 const xfs =require("./lib/xfs");
 
 
+
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
