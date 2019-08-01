f673d9934a9144277de75cfcce6f124044f54418
Yisheng Jiang <yisheng.jiang@gmail.com>
2019-08-01 12:38:21 -0700

    some new studff

--- http.js
+++ http.js
@@ -1,28 +1,27 @@
 const express = require('express')
 const app = express()
 const httpport = 8080
-var bodyParser = require('body-parser')
 const db =require("./lib/db");
 const xfs =require("./lib/xfs");
+var admin = require("./routes/admin");
 
 
-app.use('/', express.static('public'))
+app.set('view engine', 'ejs');
+
+
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-app.use(bodyParser.json());
-app.set('view engine', 'pug')
 
+app.use("/admin", admin);
+app.use('/', express.static('public'))
+app.get('/login', (req,res)=>{
+  res.render("login",{time: new Date()});
+})
 
-app.get('/:room', (req,res)=>{
- 
-    
-  res.render("")
-  res.end(); 
-});
 
 
 app.post('/files/upload', xfs.upload_handler);
@@ -31,15 +30,5 @@ app.get('/:query', (req, res) => {
     res.end(req.params.query);
 })
 
-app.get('/images', (req, res) => {
-
-})
-app.get('/images/:query', (req, res) => {
-    res.end(req.params.query);
-})
-
-app.get('/logs/:query', (req, res) => {
-    res.end(req.params.query);
-})
 
 app.listen(httpport, () => console.log(`Example app listening on port ${httpport}!`))
---
+++ public/js/tty2.js
@@ -0,0 +1,19 @@
+var Terminal = Terminal || function () {
+    const opts = Object.assign({
+        ws_url: "ws://localhost:8080",
+        api_url: "http://localhost:8081",
+        container_id: "tty",
+        parent_id: "desktop"
+    }, options);
+
+
+    var _init = function(){
+        $(".desktop").append("");
+    }
+
+
+
+
+
+
+}
