39aaa6fb6cd5f08577e6cafa1bad0f3149b1717d
Yisheng Jiang <yisheng.jiang@gmail.com>
2019-08-01 15:42:54 -0700

    editor

--- http.js
+++ http.js
@@ -8,27 +8,23 @@ var admin = require("./routes/admin");
 
 app.set('view engine', 'ejs');
 
-
-
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-
-app.use("/admin", admin);
-app.use('/', express.static('public'))
-app.get('/login', (req,res)=>{
-  res.render("login",{time: new Date()});
-})
-
-
+app.get("/file/edit", function (req, res, next){
+  res.render("editor", {url:req.params.url, context: req.params.context});
+});
 
 app.post('/files/upload', xfs.upload_handler);
 
-app.get('/:query', (req, res) => {
-    res.end(req.params.query);
-})
+app.use("/admin", admin);
+app.use('/', express.static('public'))
+
+
+
+
+
 
 
 app.listen(httpport, () => console.log(`Example app listening on port ${httpport}!`))
