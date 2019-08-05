fc837a26aa691be19133d1499e00b1b7a15b5fd1
Yisheng Jiang <yisheng.jiang@gmail.com>
2019-08-05 12:59:37 -0700

    alter table add user columns

---
+++ etc/psql/alter_table_user.sql
@@ -0,0 +1,2 @@
+alter table users add column email varchar(255) default null, add column fname varchar(255) default '', add column lname varchar(255) default '', add column avatar text default null;
+
