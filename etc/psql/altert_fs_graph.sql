bc079d4ce72201ff406d7fda634ba74f751d8100
Yisheng Jiang <yisheng.jiang@gmail.com>
2019-08-06 10:17:45 -0700

    dd

--- etc/psql/altert_fs_graph.sql
+++ etc/psql/altert_fs_graph.sql
@@ -1,3 +1,5 @@
  alter table fs_graph add column parent_node varchar(255);
  create index on fs_graph(parent_node);
 create index on fs_graph(uri);
+alter table fs_graph add column level numeric;
+create unique index on fs_graph(uri);
