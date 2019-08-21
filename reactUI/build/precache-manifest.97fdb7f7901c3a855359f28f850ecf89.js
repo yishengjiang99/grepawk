5eaea1970610ed01ed2ab75709c3ea273d3e8802
Yisheng Jiang <yisheng.jiang@gmail.com>
2019-08-20 20:26:59 -0700

    rc bu89ld

---
+++ reactUI/build/precache-manifest.97fdb7f7901c3a855359f28f850ecf89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ea8ee38fa10e1ba0d3b95d35a9de79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ee0d0a712f1f23debd1",
+    "url": "/static/css/main.7885be63.chunk.css"
+  },
+  {
+    "revision": "12052ebf2cdafe64b2a2",
+    "url": "/static/js/2.bdc7a4e8.chunk.js"
+  },
+  {
+    "revision": "5ee0d0a712f1f23debd1",
+    "url": "/static/js/main.dc1985ad.chunk.js"
+  },
+  {
+    "revision": "7b6f2350739161096299",
+    "url": "/static/js/runtime~main.f7c36e2a.js"
+  }
+]);
