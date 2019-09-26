7179e35451cd52361efb1a2b2851805a1f5bd61b
Yisheng Jiang <yisheng.jiang@gmail.com>
2019-09-26 15:07:01 -0700

    d

--- desktop/main.js
+++ desktop/main.js
@@ -1,24 +1,30 @@
 const {app,BrowserWindow,dialog} = require("electron");
+const fs = require("fs");
 
 let win;
 
 app.on("ready",()=>{
-  win = new BrowserWindow({show:false});
+  win = new BrowserWindow({show:false, backgroundColor:'gray'});
   win.loadURL(`file://${__dirname}/index.html`);
- console.log('dd');
   win.once("ready-to-show", ()=>{
-debugger;
     win.show();
-debugger;
-
-    const files = dialog.showOpenDialog({
-      properties: ['openFile']
-    });
   });
-
   win.on("closed", ()=>{ 
     win = null;
   });
 });
+app.commandLine.appendSwitch('enable-usermedia-screen-capturing');
 
+const openFile = exports.openFile = ()=>{
+  const files = dialog.showOpenDialog({
+    properties: ['openFile']
+  });
+  if(!files || !files[0]) return "";
+  const content = fs.readFileSync(files[0]).toString();
+  return content;
+}
+
+const filePutContent = exports.filePutContent = (path, content)=>{
+
+}
 
