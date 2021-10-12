adf3fedc6c5f3a499c32910b652605dfaca586c2
Yisheng Jiang <yishengjiang@yahoo.com>
2021-10-12 15:31:17 -0700

    asd

---
+++ reactUI/build/precache-manifest.cf849d9f643ab1918eafdf5d7f943da7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5e5108d5129c4b5d3ea06b32bf18f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3066f359a008b0920f57",
+    "url": "/static/css/main.e6238c69.chunk.css"
+  },
+  {
+    "revision": "8e8252474a0bdbc81c5b",
+    "url": "/static/js/2.c090b1c2.chunk.js"
+  },
+  {
+    "revision": "57c89a21effd4768389b7ddfb7dd28d7",
+    "url": "/static/js/2.c090b1c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3066f359a008b0920f57",
+    "url": "/static/js/main.c114e291.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
