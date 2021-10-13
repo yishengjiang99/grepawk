4b66a34fcb58301e6f5f18273d17e38ce859c9e4
Yisheng Jiang <yishengjiang@yahoo.com>
2021-10-13 10:30:17 -0700

    adsf

--- reactUI/src/constants.js
+++ reactUI/src/constants.js
@@ -1,9 +1,6 @@
-console.log(process.env.WS_HOST);
-export const wsRoot =
-  process.env.WS_HOST ||
-  `${window.location.protocol == "https:" ? "wss" : "ws"}://${
-    window.location.host
-  }`;
+export const wsRoot = `${
+  window.location.protocol == "https:" ? "wss" : "ws"
+}://${window.location.host}`;
 
 export const apiRoot = window.location.origin;
 
